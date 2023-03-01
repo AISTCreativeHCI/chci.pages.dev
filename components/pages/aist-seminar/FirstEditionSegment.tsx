4b9a9a403ed51272e36e1c8fda713e58f1889809
Jun Kato <i@junkato.jp>
2023-03-02 07:06:42 +0900

    Add timetable

--- components/pages/aist-seminar/FirstEditionSegment.tsx
+++ components/pages/aist-seminar/FirstEditionSegment.tsx
@@ -7,6 +7,7 @@ import {
   Image,
   List,
   Segment,
+  Table,
 } from "semantic-ui-react";
 
 import { useSiteInfo } from "../../lib/useSiteInfo";
@@ -90,7 +91,7 @@ export const FirstEditionSegment: FC = () => {
           </p>
         </>
       )}
-      <div className={styles.registration}>
+      <div className={styles.registration} id="registration">
         <Button
           icon="edit"
           color="red"
@@ -193,6 +194,85 @@ export const FirstEditionSegment: FC = () => {
           </p>
         </Grid.Column>
       </Grid>
+      <Divider />
+      <Header as="h3" content={ja ? "プログラム" : "Timetable"} />
+      <Table striped basic="very" size="small">
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>{ja ? "時刻" : "Time"}</Table.HeaderCell>
+            <Table.HeaderCell colSpan={2}>
+              {ja ? "内容" : "Program"}
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row disabled>
+            <Table.Cell>12:30</Table.Cell>
+            <Table.Cell>{ja ? "開場" : "Doors open"}</Table.Cell>
+            <Table.Cell textAlign="right">
+              {ja ? "セミナー運営" : "Organizers"}
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>13:00</Table.Cell>
+            <Table.Cell>
+              {ja
+                ? "「AIST Creative HCI Seminar」の開催経緯"
+                : "Introduction of the seminar"}
+            </Table.Cell>
+            <Table.Cell textAlign="right">
+              {ja ? "加藤 淳" : "Jun Kato"}
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>13:10</Table.Cell>
+            <Table.Cell>
+              {ja
+                ? "共催「Japan ACM SIGCHI Chapter」の紹介"
+                : "Introduction of Japan ACM SIGCHI Chapter"}
+            </Table.Cell>
+            <Table.Cell textAlign="right">
+              {ja ? "中小路 久美代" : "Nakakoji Kumiyo"}
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>13:15</Table.Cell>
+            <Table.Cell>{ja ? "招待講演 #1" : "Invited talk #1"}</Table.Cell>
+            <Table.Cell textAlign="right">Jonas Frich</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>14:15</Table.Cell>
+            <Table.Cell>{ja ? "質疑応答 #1" : "Q&A #1"}</Table.Cell>
+            <Table.Cell textAlign="right">Jonas Frich</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>14:30</Table.Cell>
+            <Table.Cell>{ja ? "招待講演 #2" : "Invited talk #2"}</Table.Cell>
+            <Table.Cell textAlign="right">Zhicong Lu</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>15:30</Table.Cell>
+            <Table.Cell>{ja ? "質疑応答 #2" : "Q&A #2"}</Table.Cell>
+            <Table.Cell textAlign="right">Zhicong Lu</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>15:45</Table.Cell>
+            <Table.Cell>
+              {ja ? "パネルディスカッション" : "Panel discussion"}
+            </Table.Cell>
+            <Table.Cell textAlign="right">
+              {ja ? "Frich, Lu, 加藤" : "Jonas, Zhicong, Jun"}
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row disabled>
+            <Table.Cell>16:25</Table.Cell>
+            <Table.Cell>{ja ? "クロージング" : "Closing"}</Table.Cell>
+            <Table.Cell textAlign="right">
+              {ja ? "セミナー運営" : "Organizers"}
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
       <Divider />
       <Header as="h3" content={ja ? "参考文献" : "References"} />
       {ja ? (
