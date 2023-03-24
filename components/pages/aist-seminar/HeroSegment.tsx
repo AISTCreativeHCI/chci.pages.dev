5bc9d5502e8f82fc797098f29b4e802ab9c88962
Jun Kato <i@junkato.jp>
2023-03-24 16:43:49 +0900

    Refactoring continues...

---
+++ components/pages/aist-seminar/HeroSegment.tsx
@@ -0,0 +1,68 @@
+import { FC, ReactNode } from "react";
+import { Divider, Header, Image, List } from "semantic-ui-react";
+
+import { useSiteInfo } from "../../lib/useSiteInfo";
+
+import styles from "./HeroSegment.module.css";
+
+interface IProps {
+  fullWidth?: boolean;
+  edition: number;
+  editionJa: string;
+  dateTime: string;
+  venue: string;
+  speakers: {
+    name: string;
+    photoPath: string;
+  }[];
+  children?: ReactNode;
+}
+
+export const HeroSegment: FC<IProps> = ({
+  fullWidth,
+  edition,
+  editionJa,
+  dateTime,
+  venue,
+  speakers,
+  children,
+}) => {
+  const { ja } = useSiteInfo();
+  return (
+    <>
+      <div
+        className={`${styles.details}${
+          fullWidth ? ` ${styles.fullwidth}` : ""
+        }`}
+      >
+        <div className={styles.title}>
+          <Header
+            as="h2"
+            content={
+              fullWidth
+                ? `#${edition} ${speakers.map((s) => s.name).join(" & ")}`
+                : `AIST Creative HCI Seminar #${edition}`
+            }
+            subheader={ja && `第${editionJa}回 AIST Creative HCI Seminar`}
+          />
+          <List>
+            <List.Item content={dateTime} />
+            <List.Item content={venue} />
+          </List>
+        </div>
+        <div className={styles.images}>
+          {speakers.map((s) => (
+            <Image
+              key={s.photoPath}
+              avatar
+              src={s.photoPath}
+              alt={`[Photo: ${s.name}]`}
+            />
+          ))}
+        </div>
+      </div>
+      {!fullWidth && children && <Divider hidden />}
+      {children}
+    </>
+  );
+};
