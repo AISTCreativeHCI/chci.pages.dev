3903b9021dc519ba3e4aa04bc2d7d0546878204f
Jun Kato <i@junkato.jp>
2023-05-22 18:15:28 +0900

    Disable registration button

--- components/pages/aist-seminar/index/Body.tsx
+++ components/pages/aist-seminar/index/Body.tsx
@@ -142,6 +142,7 @@ export const Body: FC = () => {
             }
           >
             <Button
+              disabled
               icon="edit"
               color="red"
               size="massive"
