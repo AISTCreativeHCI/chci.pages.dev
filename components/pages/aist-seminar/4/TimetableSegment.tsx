ce837568e9c411a80c9e6d8619b3fb8e2ea9f9c2
Jun Kato <i@junkato.jp>
2023-06-09 11:49:55 +0900

    Add fourth edition placeholder

---
+++ components/pages/aist-seminar/4/TimetableSegment.tsx
@@ -0,0 +1,103 @@
+import { FC, useMemo } from "react";
+import { Header, Segment, Table } from "semantic-ui-react";
+
+import { useSiteInfo } from "../../../lib/useSiteInfo";
+
+interface IProps {
+  bare?: boolean;
+}
+
+export const TimetableSegment: FC<IProps> = ({ bare }) => {
+  const { ja } = useSiteInfo();
+  const content = useMemo(
+    () => (
+      <>
+        <a id="timetable" className="anchor"></a>
+        <Header as="h2" dividing content={ja ? "プログラム" : "Timetable"} />
+        <Table striped basic="very" size="small">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>{ja ? "時刻" : "Time"}</Table.HeaderCell>
+              <Table.HeaderCell colSpan={2}>
+                {ja ? "内容" : "Program"}
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>15:00</Table.Cell>
+              <Table.Cell>
+                {ja
+                  ? "「AIST Creative HCI Seminar」の開催経緯"
+                  : "Introduction of the seminar"}
+              </Table.Cell>
+              <Table.Cell textAlign="right">
+                {ja ? "加藤 淳" : "Jun Kato"}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>15:05</Table.Cell>
+              <Table.Cell>
+                {ja ? "招待講演 #1 / Papers" : "Invited talk #1 / Papers"}
+              </Table.Cell>
+              <Table.Cell textAlign="right">
+                {ja ? "鳴海 紘也" : "Koya Narumi"}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>15:35</Table.Cell>
+              <Table.Cell>
+                {ja
+                  ? "招待講演 #2 / Case Studies & Doctoral Consortium"
+                  : "Invited talk #2 / Case Studies & Doctoral Consortium"}
+              </Table.Cell>
+              <Table.Cell textAlign="right">
+                {ja ? "矢倉 大夢" : "Hiromu Yakura"}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>16:05</Table.Cell>
+              <Table.Cell>
+                {ja
+                  ? "招待講演 #3 / Late-Breaking Work & Interactivity"
+                  : "Invited talk #3 / Late-Breaking Work & Interactivity"}
+              </Table.Cell>
+              <Table.Cell textAlign="right">
+                {ja ? "元村 愛美" : "Motomura Ami"}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>16:35</Table.Cell>
+              <Table.Cell>
+                {ja
+                  ? "Student Competitions & Special Interest Group"
+                  : "Student Competitions & Special Interest Group"}
+              </Table.Cell>
+              <Table.Cell textAlign="right">
+                {ja ? "加藤 淳" : "Jun Kato"}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>16:45</Table.Cell>
+              <Table.Cell>
+                {ja ? "パネルディスカッション" : "Panel discussion"}
+              </Table.Cell>
+              <Table.Cell textAlign="right">
+                {ja ? "鳴海, 矢倉, 元村, 加藤" : "Koya, Hiromu, Ami, Jun"}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row disabled>
+              <Table.Cell>16:55</Table.Cell>
+              <Table.Cell>{ja ? "クロージング" : "Closing"}</Table.Cell>
+              <Table.Cell textAlign="right">
+                {ja ? "セミナー運営" : "Organizers"}
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </>
+    ),
+    []
+  );
+  return bare ? content : <Segment basic>{content}</Segment>;
+};
