c5cf7b2161042a19af01a3ff80774e4718589926
Jun Kato <i@junkato.jp>
2023-06-20 05:38:11 +0900

    Update references

--- components/pages/aist-seminar/4/ReferencesSegment.tsx
+++ components/pages/aist-seminar/4/ReferencesSegment.tsx
@@ -12,6 +12,12 @@ export const ReferencesSegment: FC<IProps> = ({ bare }) => (
     description={null}
     publications={[
       {
+        pre: "Jon E. Froehlich, Anke M. Brock, Anat Caspi, João Guerreiro, Kotaro Hara, Reuben Kirkham, Johannes Schöning, and Benjamin Tannert. 2019",
+        title: "Grand Challenges in Accessible Maps",
+        post: "Interactions 26, 2 (March - April 2019). Association for Computing Machinery, New York, NY, USA, 78–81.",
+        url: "https://doi.org/10.1145/3301657",
+      },
+      {
         pre: "Riku Arakawa, Karan Ahuja, Kristie Mak, Gwendolyn Thompson, Sam Shaaban, Oliver Lindhiem, and Mayank Goel",
         title:
           "LemurDx: Using Unconstrained Passive Sensing for an Objective Measurement of Hyperactivity in Children with no Parent Input",
