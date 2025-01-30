f5a3d92c5fdb96ca45623941ba56ec743ec444b2
Jun Kato <i@junkato.jp>
2025-01-30 21:12:46 +0100

    Update venue map

--- components/pages/chap2025paris/VenueSegment.tsx
+++ components/pages/chap2025paris/VenueSegment.tsx
@@ -22,7 +22,7 @@ export const VenueSegment: FC<
       <Header
         as="h2"
         dividing={typeof dividing === "boolean" ? dividing : true}
-        content="Onsite venue"
+        content="Venues"
         icon={icon}
       />
       <Grid stackable>
@@ -32,13 +32,17 @@ export const VenueSegment: FC<
             <GMapsIframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2626.6694011238196!2d2.3438620767089593!3d48.826368971327994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e67213a6b6c81f%3A0x61fe1080582e2944!2sInria!5e0!3m2!1sja!2sfr!4v1737122564750!5m2!1sja!2sfr" />
           </Grid.Column>
           <Grid.Column>
-            <Header
-              as="h3"
-              dividing
-              color="orange"
-              content="Workshops (tentative)"
-            />
-            <GMapsIframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14845.534671970461!2d2.3940991310194253!3d48.87070775197906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66da268f03acf%3A0x58c65d39a00163e5!2sGobelins%20Paris%20-%20Campus%20Gambetta!5e0!3m2!1sja!2sfr!4v1738220248230!5m2!1sja!2sfr" />
+            <Header as="h3" dividing color="orange" content="Workshops" />
+            <Grid stackable>
+              <Grid.Row columns={2}>
+                <Grid.Column>
+                  <GMapsIframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14845.534671970461!2d2.3940991310194253!3d48.87070775197906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66da268f03acf%3A0x58c65d39a00163e5!2sGobelins%20Paris%20-%20Campus%20Gambetta!5e0!3m2!1sja!2sfr!4v1738220248230!5m2!1sja!2sfr" />
+                </Grid.Column>
+                <Grid.Column>
+                  <GMapsIframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d780.5976014091748!2d2.3535760018588396!3d48.846636454187916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671faadc9abf1%3A0xb9f0dc922cc91d!2sSorbonne%20University!5e0!3m2!1sja!2sfr!4v1738265609478!5m2!1sja!2sfr" />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           </Grid.Column>
         </Grid.Row>
       </Grid>
