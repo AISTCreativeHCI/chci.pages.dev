2381d5e6b695c933b4cc02f5888aa3dbdc1cce86
Jun Kato <i@junkato.jp>
2025-02-23 19:19:29 +0100

    Refactoring Alertbox

--- components/pages/aist-seminar/Alertbox.tsx
+++ components/pages/aist-seminar/Alertbox.tsx
@@ -1,17 +1,49 @@
-import { FC, ReactNode } from "react";
+import { CSSProperties, FC, ReactNode } from "react";
 
 import styles from "./Alertbox.module.css";
 
 interface IProps {
   children?: ReactNode;
   text?: ReactNode;
+  styling: {
+    color?: string;
+    backgroundColor?: string;
+    borderColor?: string;
+  };
 }
 
-export const Alertbox: FC<IProps> = ({ children, text }) => {
+export const Alertbox: FC<IProps> = ({
+  children,
+  text,
+  styling: {
+    color = "#a33",
+    backgroundColor = "#fff6f6",
+    borderColor = "#fee",
+  },
+}) => {
   return (
-    <div className={styles.alertbox}>
+    <div
+      className={styles.alertbox}
+      style={
+        {
+          "--color": color,
+          "--background-color": backgroundColor,
+        } as CSSProperties
+      }
+    >
       {children}
-      {text && <div className={styles.alert}>{text}</div>}
+      {text && (
+        <div
+          className={styles.alert}
+          style={
+            {
+              "--border-color": borderColor,
+            } as CSSProperties
+          }
+        >
+          {text}
+        </div>
+      )}
     </div>
   );
 };
